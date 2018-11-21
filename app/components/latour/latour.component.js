280e155a88b5a459a2d2f3b63f08e034447b9001
Johannisson <andreas.johannisson@gmail.com>
2018-11-21 22:20:28 +0100

    no message

--- app/components/latour/latour.component.js
+++ app/components/latour/latour.component.js
@@ -11,4 +11,41 @@ LatourController.$inject = [];
 
 function LatourController() {
     var vm = this;
+
+    this.dividend = [
+        { year: 1986, ordinary: 0.01 },
+        { year: 1987, ordinary: 0.02, extra: 0.05 },
+        { year: 1988, ordinary: 0.02, extra: 0.02 },
+        { year: 1989, ordinary: 0.05 },
+        { year: 1990, ordinary: 0.05 },
+        { year: 1991, ordinary: 0.07 },
+        { year: 1992, ordinary: 0.16 },
+        { year: 1993, ordinary: 0.09 },
+        { year: 1994, ordinary: 0.09, extra: 0.02 },
+        { year: 1995, ordinary: 0.11 },
+        { year: 1996, ordinary: 0.15 },
+        { year: 1997, ordinary: 0.18 },
+        { year: 1998, ordinary: 0.20 },
+        { year: 1999, ordinary: 0.22 },
+        { year: 2000, ordinary: 0.29 },
+        { year: 2001, ordinary: 0.41 },
+        { year: 2002, ordinary: 0.46, extra: 0.25 },
+        { year: 2003, ordinary: 0.46, extra: 0.13 },
+        { year: 2004, ordinary: 0.50 },
+        { year: 2005, ordinary: 0.50 },
+        { year: 2006, ordinary: 0.58 },
+        { year: 2007, ordinary: 0.71 },
+        { year: 2008, ordinary: 0.88 },
+        { year: 2009, ordinary: 0.94 },
+        { year: 2010, ordinary: 0.69 },
+        { year: 2011, ordinary: 0.94 },
+        { year: 2012, ordinary: 1.13, extra: 0.38 },
+        { year: 2013, ordinary: 1.25 },
+        { year: 2014, ordinary: 1.38 },
+        { year: 2015, ordinary: 1.50 },
+        { year: 2016, ordinary: 1.69 },
+        { year: 2017, ordinary: 2.00 },
+        { year: 2018, ordinary: 2.25 }
+
+    ]
 }
