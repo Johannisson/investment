14c4058c9022c11a390a5e526f968b2efaa8883f
Johannisson <andreas.johannisson@gmail.com>
2018-12-01 10:28:39 +0100

    no message

--- app/components/report/report.component.js
+++ app/components/report/report.component.js
@@ -19,30 +19,45 @@ function ReportController() {
                 {
                     data: castellum,
                     quantity: 50,
+                    value: 7960,
                     delta: 50
                 }, {
                     data: handelsbanken,
                     quantity: 60,
+                    value: 6015,
                     delta: 60
                 }, {
                     data: husqvarna,
                     quantity: 120,
+                    value: 8602,
                     delta: 120
                 }, {
                     data: investor,
                     quantity: 30,
+                    value: 11961,
                     delta: 30
                 }, {
                     data: latour,
-                    quantity: 220
+                    quantity: 220,
+                    value: 25036
                 }, {
                     data: sagax,
-                    quantity: 660
+                    quantity: 660,
+                    value: 21648,
                 }, {
                     data: swedbank,
-                    quantity: 110
+                    quantity: 110,
+                    value: 23265,
                 }
             ]
         }
     };
+
+    vm.getTotal = function () {
+        var sum = 0;
+        vm.year2018.november.stocks.forEach(function(stock) {
+            sum += stock.value;
+        });
+        return sum;
+    }
 }
