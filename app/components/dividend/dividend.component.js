038af999706349c1581c0f10422f15c7bf155fa5
Johannisson <andreas.johannisson@gmail.com>
2018-12-09 22:27:36 +0100

    no message

--- app/components/dividend/dividend.component.js
+++ app/components/dividend/dividend.component.js
@@ -15,21 +15,31 @@ function DividendController() {
     var vm = this;
 
     vm.months = [
-        { id: 'january', name: 'Januari' },
-        { id: 'february', name: 'Februari' },
-        { id: 'march', name: 'Mars' },
-        { id: 'april', name: 'April' },
-        { id: 'may', name: 'Maj' },
-        { id: 'june', name: 'Juni' },
-        { id: 'july', name: 'Juli' },
-        { id: 'august', name: 'Augusti' },
-        { id: 'september', name: 'September' },
-        { id: 'october', name: 'Oktober' },
-        { id: 'november', name: 'November' },
-        { id: 'december', name: 'December' }
+        { id: 'january', name: 'Januari', short: 'Jan' },
+        { id: 'february', name: 'Februari', short: 'Feb' },
+        { id: 'march', name: 'Mars', short: 'Mars' },
+        { id: 'april', name: 'April', short: 'April' },
+        { id: 'may', name: 'Maj', short: 'Maj' },
+        { id: 'june', name: 'Juni', short: 'Juni' },
+        { id: 'july', name: 'Juli', short: 'Juli' },
+        { id: 'august', name: 'Augusti', short: 'Aug' },
+        { id: 'september', name: 'September', short: 'Sept' },
+        { id: 'october', name: 'Oktober', short: 'Okt' },
+        { id: 'november', name: 'November', short: 'Nov' },
+        { id: 'december', name: 'December', short: 'Dec' }
     ];
 
     vm.assets = assets;
+
+    vm.getSum = function(month) {
+        var sum = 0;
+        vm.assets.forEach(function(asset) {
+            if (asset.dividends[month.id] !== undefined) {
+                sum += asset.dividends[month.id] * asset.quantity
+            }
+        });
+        return sum;
+    }
 
     vm.getTotal = function () {
         var total = 0;
