e4dc6b56b1f01fd4bf623708d08f850d48e1a254
Johannisson <andreas.johannisson@gmail.com>
2018-12-11 16:41:55 +0100

    no message

--- app/components/milestones/milestones.component.js
+++ app/components/milestones/milestones.component.js
@@ -11,4 +11,10 @@ MilestonesController.$inject = [];
 
 function MilestonesController() {
     var vm = this;
+
+    vm.monthly = [];
+    vm.monthly.push({ goal: 500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 1000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 1500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 2000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
 }
