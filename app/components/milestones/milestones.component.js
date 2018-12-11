8c8e6a128633f7071018a433806cf1cd479d87bc
Johannisson <andreas.johannisson@gmail.com>
2018-12-11 17:26:34 +0100

    no message

--- app/components/milestones/milestones.component.js
+++ app/components/milestones/milestones.component.js
@@ -13,8 +13,14 @@ function MilestonesController() {
     var vm = this;
 
     vm.monthly = [];
-    vm.monthly.push({ goal: 500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
-    vm.monthly.push({ goal: 1000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
-    vm.monthly.push({ goal: 1500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 500, months:  [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 1000, months: [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 1500, months: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
     vm.monthly.push({ goal: 2000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 2500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 3000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 3500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 4000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 4500, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+    vm.monthly.push({ goal: 5000, months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
 }
