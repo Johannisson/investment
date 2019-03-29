e8cbb9ed0447dad5460d93b8c281a66a3eaffa83
Johannisson <andreas.johannisson@gmail.com>
2019-03-29 20:31:20 +0100

    no message

--- app/components/dividends/dividends.component.js
+++ app/components/dividends/dividends.component.js
@@ -13,66 +13,15 @@ function DividendsController() {
     var vm = this;
 
     vm.dividends = [
-        {
-            date: '2019-01-04',
-            quantity: 20,
-            amount: 11.00,
-            asset: kungsleden
-        },
-        {
-            date: '2019-01-04',
-            quantity: 400,
-            amount: 200.00,
-            asset: sagax
-        },
-        {
-            date: '2019-01-04',
-            quantity: 12,
-            amount: 27.36,
-            tax: 7.38,
-            asset: tryg,
-            last: true
-        },
-        {
-            date: '2019-02-08',
-            quantity: 10,
-            amount: 50.00,
-            asset: akelius,
-        },
-        {
-            date: '2019-02-13',
-            type: 'arbitrage',
-            quantity: 50,
-            amount: 137.00,
-            asset: investor_a,
-            last: true
-        },
-        {
-            date: '2019-03-02',
-            quantity: 10,
-            amount: 33.32,
-            tax: 4.99,
-            asset: pfizer,
-        },
-        {
-            date: '2019-03-06',
-            quantity: 4,
-            amount: 22.99,
-            tax: 3.44,
-            asset: autoliv,
-        },
-        {
-            date: '2019-03-28',
-            quantity: 99,
-            amount: 693.00,
-            asset: axfood,
-        },
-        {
-            date: '2019-03-28',
-            quantity: 90,
-            amount: 274.50,
-            asset: castellum,
-            last: true
-        }
+        { date: '2019-01-04',  asset: kungsleden,        quantity: 20,   amount: 11.00,   tax: 0,     type: '',           last: false },
+        { date: '2019-01-04',  asset: sagax,             quantity: 400,  amount: 200.00,  tax: 0,     type: '',           last: false },
+        { date: '2019-01-04',  asset: tryg,              quantity: 12,   amount: 27.36,   tax: 7.38,  type: '',           last: true  },
+        { date: '2019-02-08',  asset: akelius,           quantity: 10,   amount: 50.00,   tax: 0,     type: '',           last: false },
+        { date: '2019-02-13',  asset: investor_a,        quantity: 50,   amount: 137.00,  tax: 0,     type: 'arbitrage',  last: true  },
+        { date: '2019-03-02',  asset: pfizer,            quantity: 10,   amount: 33.32,   tax: 4.99,  type: '',           last: false },
+        { date: '2019-03-06',  asset: autoliv,           quantity: 4,    amount: 22.99,   tax: 3.44,  type: '',           last: false },
+        { date: '2019-03-28',  asset: axfood,            quantity: 99,   amount: 693.00,  tax: 0,     type: '',           last: false },
+        { date: '2019-03-28',  asset: castellum,         quantity: 90,   amount: 274.50,  tax: 0,     type: '',           last: false },
+        { date: '2019-03-29',  asset: industrivarden_a,  quantity: 13,   amount: 56.00,   tax: 0,     type: 'arbitrage',  last: true  }
     ];
 }
